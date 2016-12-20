8c46ba6e870d60edc1b997c2488a9854ffe3fb27
ghusar <hghusarr@gmail.com>
2016-12-20 13:35:17 +0530

    front end updates

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -10,4 +10,20 @@ $(document).ready(function () {
                 console.log(data.data);
             });
     });
+
+    $('#searchIcon').click(function(){
+        $('nav').animate({height:'20%',fontSize:'35px',paddingTop:'25px'},300);
+        $('#searchIcon').hide(300);
+        $('#one').show(300);
+        $('#two').hide(300);
+
+    });
+
+    $('.input-field > i').click(function(){
+        $('#one').hide(300);
+        $('#two').show(300);
+        $('#searchIcon').show(300);
+        $('nav').animate({height:'30%',fontSize:'45px',paddingTop:'40px'},300);
+
+    });
 });
